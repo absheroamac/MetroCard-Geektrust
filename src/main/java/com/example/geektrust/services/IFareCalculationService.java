32e90e518318c6e1137c2cb77b725372c1b3f202
Absher K <absheroamac@gmail.com>
2024-03-27 21:09:06 +0530

    Added Comments

--- src/main/java/com/example/geektrust/services/IFareCalculationService.java
+++ src/main/java/com/example/geektrust/services/IFareCalculationService.java
@@ -5,10 +5,30 @@ import com.example.geektrust.entities.Passanger;
 import com.example.geektrust.utils.Bill;
 
 public interface IFareCalculationService {
-
+    /**
+     * Calculate the fare based on the Passanger type
+     * 
+     * @param passanger Passenger type
+     * @return return calculated fare
+     */
     public int calculateFare(Passanger passanger);
 
+    /**
+     * Will return the Bill, which call calculateFare method to get the fare and
+     * call checkForDiscount method
+     * 
+     * @param passanger Passenger Type
+     * @param metroCard MetroCard object
+     * @return Return Bill object with fare,discount and payable(dare-discount)
+     */
+
     public Bill getBill(Passanger passanger, MetroCard metroCard);
+
+    /**
+     * Method will set JourneyService
+     * 
+     * @param journeyService
+     */
 
     public void setJourneyService(IJourneyService journeyService);
 
