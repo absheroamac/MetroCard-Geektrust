c4ed01f0a2c9f6f4faa38b2a3dbf4a7496fe9fa4
Absher K <absheroamac@gmail.com>
2024-03-30 19:41:25 +0530

    Added more comments

--- src/main/java/com/example/geektrust/services/IJourneyService.java
+++ src/main/java/com/example/geektrust/services/IJourneyService.java
@@ -14,11 +14,39 @@ import com.example.geektrust.utils.PassengerSummary;
 
 public interface IJourneyService {
 
+    /**
+     * Create Journey object based on the input
+     * 
+     * @param id        id of the metroCard
+     * @param passanger Passenger Type
+     * @param from      From Station type
+     */
     public void createJourney(String id, Passanger passanger, Station from);
+
+    /**
+     * Get Station Summary
+     * 
+     * @param station Station type
+     * @return Return list of StationSummary objects from the stationType
+     */
 
     public CollectionSummary getStationSummary(StationType station);
 
+    /**
+     * Collect Journeys by card id
+     * 
+     * @param id id of the MetroCard
+     * @return List of Journey objects of the metroCard id.
+     */
+
     public List<Journey> getJourneysOf(String id);
+
+    /**
+     * collect Passenger Summarys
+     * 
+     * @param station Station type
+     * @return Return Map of PassengerType and PassengerSummary objects
+     */
 
     public Map<PassangerType, PassengerSummary> getPassengerSummary(StationType station);
 
